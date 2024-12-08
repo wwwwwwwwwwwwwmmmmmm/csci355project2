c68a828f9e87b20eeb79dfdfdd81a1c3a7f7800f
wwwwwwwwwwwwwmmmmmm <wwwwmmmm159262@gmail.com>
2024-12-08 13:56:00 -0500

    fix: add old project routing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         {/* Link to home.html */}
-        <a href="/old/Home.html" target="_blank" rel="noopener noreferrer">
+        <a href="/Home.html" target="_blank" rel="noopener noreferrer">
             Go to Home Page
         </a>
     </div>
